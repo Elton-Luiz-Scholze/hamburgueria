9db8c64d0fb9bc48ddbd2752daf84db7ccd0f9d0
Elton Luiz Scholze <elscholze@gmail.com>
2022-10-01 12:31:20 -0300

    fix: fix App.jsx

---
+++ react-entrega-hamburgueria-elton-luiz-scholze/src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ProductsList } from "./components/ProductsList";
+import logo from "./assets/logo.svg";
+import { Cart } from "./components/Cart";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  // const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentSale, setCurrentSale] = useState([]);
+  // const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    fetch("https://hamburgueria-kenzie-json-serve.herokuapp.com/products")
+      .then((res) => res.json())
+      .then((res) => setProducts(res))
+      .catch((err) => console.log(err));
+  }, []);
+  // function showProducts() {}
+
+  function handleClick(productId) {}
+
+  return (
+    <div className="app">
+      <header>
+        <div className="containerHeader">
+          <img src={logo} alt="Logo Burguer Kenzie" />
+          <div>
+            <input type="text" placeholder="Digitar Pesquisa" />
+            <button type="button">Pesquisar</button>
+          </div>
+        </div>
+      </header>
+      <div className="container">
+        <ProductsList products={products} handleClick={handleClick} />
+        <Cart currentSale={currentSale} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
