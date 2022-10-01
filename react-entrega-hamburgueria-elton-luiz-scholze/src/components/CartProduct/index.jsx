247bca6cb04cb674fbe0c5bf6362935f7e61bb4c
Elton Luiz Scholze <elscholze@gmail.com>
2022-10-01 12:40:20 -0300

    fix: fix CartProduct/index.jsx

--- react-entrega-hamburgueria-elton-luiz-scholze/src/components/CartProduct/index.jsx
+++ react-entrega-hamburgueria-elton-luiz-scholze/src/components/CartProduct/index.jsx
@@ -1,1 +1,13 @@
-export function CartProduct() {}
+export function CartProduct({ name, category, img, id }) {
+  return (
+    <li>
+      <img src={img} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{category}</p>
+        <span>Quantidade: </span>
+      </div>
+      <button>Remover</button>
+    </li>
+  );
+}
