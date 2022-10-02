246ac278dc8a3c69dee62de1e2c0ae7d4b0bb8b9
Elton Luiz Scholze <elscholze@gmail.com>
2022-10-01 21:02:11 -0300

    fix: Product/index.jsx, Product/style.js

--- react-entrega-hamburgueria-elton-luiz-scholze/src/components/Product/index.jsx
+++ react-entrega-hamburgueria-elton-luiz-scholze/src/components/Product/index.jsx
@@ -1,11 +1,17 @@
+import { Button, Card, CardInfo } from "./style";
+
 export function Product({ name, category, price, img, id, handleClick }) {
   return (
-    <li>
-      <img src={img} alt={name} />
-      <h2>{name}</h2>
-      <p>{category}</p>
-      <span>R$ {price.toFixed(2).replace(".", ",")}</span>
-      <button onClick={() => handleClick(id)}>Adicionar</button>
-    </li>
+    <Card>
+      <figure>
+        <img src={img} alt={name} />
+      </figure>
+      <CardInfo>
+        <h2>{name}</h2>
+        <p>{category}</p>
+        <span>R$ {price.toFixed(2).replace(".", ",")}</span>
+        <Button onClick={() => handleClick(id)}>Adicionar</Button>
+      </CardInfo>
+    </Card>
   );
 }
